679eb8ba00f5a3b734d223a1c526fde86807afb4
NW-Lab <g.tauchi@gmail.com>
2025-11-01 19:48:21 +0900

    test

--- node/m5roller485-node/m5roller485-node.mcu.js
+++ node/m5roller485-node/m5roller485-node.mcu.js
@@ -1,13 +1,3 @@
-module.exports = function(RED) {
-  // 通常版 Node-RED 用のスタブ。
-  // このノードは Node-RED MCU( Moddable ) 専用だよ。
-  function M5Roller485Node(config) {
-    RED.nodes.createNode(this, config);
-    this.status({ fill: "red", shape: "ring", text: "MCU only" });
-    this.error("This node runs only on Node-RED MCU (Moddable). It cannot run in standard Node-RED.");
-  }
-  RED.nodes.registerType("m5roller485-node", M5Roller485Node);
-};
 /*
  * Copyright (c) 2025 NW-Lab
  *
